228d51f1af94ef0942735779a55d78fc49cf2177
Bihan Viranga <bihanviranga@gmail.com>
2020-08-23 11:41:28 +0530

    Work on navbar and main title

--- librarycore/urls.py
+++ librarycore/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 import librarycore.views as views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
     path('books', views.books, name='books'),
 ]
--- librarycore/views.py
+++ librarycore/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from . import models
 
 def index(request):
-    return render(request, "librarycore/index.html")
+    return render(request, "librarycore/index.html", context={"currentNav": "index"})
 
 def books(request):
     books = models.Book.objects.all()
-    return render(request, "librarycore/books.html", context={"books":books})
+    return render(request, "librarycore/books.html", context={"currentNav": "books", "books":books})
 
