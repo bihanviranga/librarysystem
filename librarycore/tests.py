6b42b84743a1b925dac4782094273289850b4148
Bihan Viranga <bihanviranga@gmail.com>
2020-08-29 17:43:47 +0530

    Added tests for post endpoints

--- librarycore/tests.py
+++ librarycore/tests.py
@@ -4,10 +4,6 @@ from django.test.utils import setup_test_environment
 from django.urls import reverse
 from librarycore import models
 
-# TODO: Test POST methods written by hand:
-# views.Books.post
-# views.BookInstanceCreate.post
-# views.BookInstanceUpdate.post
 # TODO: Test NotFound errors in GET and DELETE and UPDATE
 
 def getBooks(count):
@@ -41,6 +37,14 @@ class BookViewTests(TestCase):
     def setup(self):
         setup_test_environment()
 
+    def test_createBookPostRequest(self):
+        url = reverse('book-create')
+        postDict = {'bookName': 'testingBook', 'bookAuthor':'testingAuthor'}
+        response = self.client.post(url, postDict)
+        bookFromDb = models.Book.objects.all()[0]
+        self.assertEqual(bookFromDb.bookName, postDict['bookName'])
+        self.assertEqual(bookFromDb.bookAuthor, postDict['bookAuthor'])
+
     def test_booksPageHasCurrentNavSet(self):
         url = reverse('books')
         response = self.client.get(url)
@@ -61,12 +65,31 @@ class BookViewTests(TestCase):
         self.assertEqual(response.context['instanceTypes'], models.BookInstance.INSTANCE_TYPE_CHOICES)
 
 class BookInstanceViewTests(TestCase):
-        def setup(self):
-            setup_test_environment()
+    def setup(self):
+        setup_test_environment()
 
-        def test_bookInstancePageHasInstanceTypes(self):
-            book1 = getBooks(1)[0]
-            instance = getBookInstances(1, book1)[0]
-            url = reverse('instance-detail', args=[instance.instanceSerialNum])
-            response = self.client.get(url)
-            self.assertEqual(response.context['instanceTypes'], models.BookInstance.INSTANCE_TYPE_CHOICES)
+    def test_createBookInstancePostRequest(self):
+        book1 = getBooks(1)[0]
+        url = reverse('instance-create')
+        postDict = {'instanceType': random.choice(models.BookInstance.INSTANCE_TYPE_CHOICES[0]), 'bookId': str(book1.id)}
+        response = self.client.post(url, postDict)
+        instanceFromDb = models.BookInstance.objects.all()[0]
+        self.assertEqual(instanceFromDb.instanceType, postDict['instanceType'])
+        self.assertEqual(instanceFromDb.instanceBook, book1)
+
+    def test_updateBookInstancePostRequest(self):
+        book1 = getBooks(1)[0]
+        instance1 = getBookInstances(1, book1)[0]
+        url = reverse('instance-update', args=[instance1.instanceSerialNum])
+        postInstanceType = random.choice(models.BookInstance.INSTANCE_TYPE_CHOICES)[0]
+        postDict = {'instanceSerialNum': instance1.instanceSerialNum, 'instanceType': postInstanceType}
+        response = self.client.post(url, postDict)
+        instanceFromDb = models.BookInstance.objects.get(pk=postDict['instanceSerialNum'])
+        self.assertEqual(instanceFromDb.instanceType, postDict['instanceType'])
+
+    def test_bookInstancePageHasInstanceTypes(self):
+        book1 = getBooks(1)[0]
+        instance = getBookInstances(1, book1)[0]
+        url = reverse('instance-detail', args=[instance.instanceSerialNum])
+        response = self.client.get(url)
+        self.assertEqual(response.context['instanceTypes'], models.BookInstance.INSTANCE_TYPE_CHOICES)
