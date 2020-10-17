97a5b46f3d62f70220fa1f753eec3fb318493d49
Bihan Viranga <bihanviranga@gmail.com>
2020-10-17 20:15:18 +0530

    Deleted old views stub

--- librarycore/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, redirect
-from django.views import View
-from django.views.generic import DeleteView, DetailView, UpdateView, ListView
-from django.urls import reverse_lazy
-from django.contrib.auth.models import User
-from django.contrib.auth.mixins import LoginRequiredMixin
-from . import models
-from .mixins import UserIsAdminMixin
-
